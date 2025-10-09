e578f5c595454ac33556e4d1e7caf6b6f3f8e705
VanThanh34 <vvt1304@gmail.com>
2025-10-09 16:10:02 +0700

    Sửa lại giao diện và menu

--- case_study_module_4/src/main/java/com/example/case_study_module_4/controller/RestaurantController.java
+++ case_study_module_4/src/main/java/com/example/case_study_module_4/controller/RestaurantController.java
@@ -5,22 +5,19 @@ import com.example.case_study_module_4.entity.Restaurant;
 import com.example.case_study_module_4.service.IFoodService;
 import com.example.case_study_module_4.service.IRestaurantService;
 import com.example.case_study_module_4.service.impl.FoodService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
+@RequiredArgsConstructor
 public class RestaurantController {
-    @Autowired
-    private IRestaurantService restaurantService;
 
-
-
-    @Autowired
-    private FoodService foodService;
-
+    private final IRestaurantService restaurantService;
     @GetMapping("/restaurants")
     public String showFoodList(Model model) {
         model.addAttribute("restaurants", restaurantService.findAll());
