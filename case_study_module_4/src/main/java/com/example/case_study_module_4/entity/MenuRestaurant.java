575941126bfc91420f2615c8b9ca63499c0d81ab
VanThanh34 <vvt1304@gmail.com>
2025-10-09 15:53:37 +0700

    Sửa lại Entity MenuRestaurant

--- case_study_module_4/src/main/java/com/example/case_study_module_4/entity/MenuRestaurant.java
+++ case_study_module_4/src/main/java/com/example/case_study_module_4/entity/MenuRestaurant.java
@@ -1,16 +1,15 @@
+/*
 package com.example.case_study_module_4.entity;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-@Entity(name = "menu_restaurant")
+@Entity
+@Table(name = "menu_restaurant")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -30,4 +29,40 @@ public class MenuRestaurant {
     private boolean isAvailable;
     private int position;
     private LocalDateTime createDate;
+}*/
+
+
+
+package com.example.case_study_module_4.entity;
+import jakarta.persistence.*;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "menu_restaurant")
+public class MenuRestaurant {
+    @EmbeddedId
+    private MenuRestaurantId id;
+    @ManyToOne
+    @MapsId("resId") // ánh xạ với resId trong EmbeddedId
+    @JoinColumn(name = "res_id")
+    private Restaurant restaurant;
+    @ManyToOne
+    @MapsId("foodId") // ánh xạ với foodId trong EmbeddedId
+    @JoinColumn(name = "food_id")
+    private Food food;
+    private Boolean isAvailable = true;
+    private Integer position;
 }
+
+
+
+
+
+
+
+
+
